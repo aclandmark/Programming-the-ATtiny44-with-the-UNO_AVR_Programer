d4e80db7d73cb3a7807772717046449b052f6088
aclandmark <mark.osborne15@btinternet.com>
2020-09-24 15:46:12 +0100

    Create USI_subroutines.c

---
+++ Cal_ATtiny/Resources/USI_subroutines.c
@@ -0,0 +1,72 @@
+
+unsigned char ReverseByte (unsigned char x) {
+	x = ((x >> 1) & 0x55) | ((x << 1) & 0xAA);
+	x = ((x >> 2) & 0x33) | ((x << 2) & 0xCC);
+	x = ((x >> 4) & 0x0F) | ((x << 4) & 0xF0);
+return x;}
+
+
+
+
+
+void Char_to_USI(unsigned char Txdata){
+	
+	Txdata = ReverseByte(Txdata);
+	Start_384_clock;									//Start clock
+		
+	while (!(char_transmitted));						//wait for USI counter overflow
+	char_transmitted = 0;
+	USISR = 0xF0 | (0x10 - 0x05);						//First half of data
+	USIDR = (Txdata >> 2) | 0x80;
+
+	while (!(char_transmitted));						//wait for USI counter overflow
+	char_transmitted = 0;
+	USISR = 0xF0 | (0x10 - 0x05);						//Second half of data
+	USIDR = (Txdata << 3) | 0x07;
+
+	while (!(char_transmitted));						//wait for USI counter overflow
+	char_transmitted = 0;
+	TCCR0B = 0;	}										//halt baud rate clock
+
+
+
+void Initialise_USI_Tx (void)
+	{	
+		DDRA &= (!(1 << DDA6));							//Put WPU on PA6 (DI pin)
+		PORTA |= 1 << PA6;
+		
+		USICR = 0;										//Reset USI  EXTRA line
+		Set_Baud_Rate_384;
+		TIFR0 = (1 << OCF0A);							//Clear spurious interrupts
+		TCNT0 = 0;										//Clear TCNT0
+		TIMSK0 |= (1 << OCIE0A);						//Enable interrupt on output compare
+		
+		USICR |= ( 1 << USIOIE);						//Enable USI counter interrupt
+		USICR |= (1 << USIWM0);							//Select USI 3-wire mode
+		USICR |= (1 << USICS0);							//Select USI clock source (timer0 compare match)
+		DDRA |= (1 << DDA5);							//Configure DO (PA5) as output
+		USIDR = 0xFF;									//Load USIDR with 0xFF
+		USISR = 0xFF;}									//clear bit counter
+		
+
+
+
+/************************************************************************************************/
+unsigned char Char_from_USI (void)
+{
+	DDRA &= (~((1 << DDA4) | (1 << DDA5) | (1 << DDA6)));				//Set USI IO to WPU
+	PORTA |= (1 << PA4) | (1 << PA5) | (1 << PA6);
+	USICR = 0;															//Reset USI
+	
+	GIFR |= 1 << PCIF0;													//clear spurious interrupts on DI pin
+	GIMSK |= 1 << PCIE0;												//Set PCI on DI pin
+	PCMSK0 |= 1 << PCINT6;
+	
+	while(!(char_received));											//Wait for USI overflow ISR
+	char_received = 0;
+	Initialise_USI_Tx ();												//Leave USI ready to transmit char
+return (ReverseByte(USIBR));}											//Take char from buffer register
+
+
+
+
