136081dfa018f2b7ac9adb3b0271302a28580850
aclandmark <mark.osborne15@btinternet.com>
2020-09-24 15:45:55 +0100

    Create Project.h

---
+++ Cal_ATtiny/Cal_ATtiny/Project.h
@@ -0,0 +1,7 @@
+
+
+
+#include "../Resources/ATtiny_cal.h"
+#include "../Resources/USI_subroutines.c"
+#include "../Resources/ASKII_and_timer_subroutines.c"
+#include "../Resources/cal_subroutines.c"
