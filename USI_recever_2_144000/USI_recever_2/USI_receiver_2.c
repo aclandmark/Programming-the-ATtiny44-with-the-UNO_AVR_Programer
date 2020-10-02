a1eb8d8870874befa5024dd0f27a03a25f464031
aclandmark <mark.osborne15@btinternet.com>
2020-10-02 11:59:57 +0100

    Create USI_receiver_2.c

---
+++ USI_recever_2_144000/USI_recever_2/USI_receiver_2.c
@@ -0,0 +1,79 @@
+							
+
+#include "Project.h"
+
+void Timer_T1_sub(void);
+
+
+
+volatile char test,  bit_flag, test_2;
+
+
+int main (void)
+{	
+	unsigned char Keypress = 'A', char_buffer[47];					//Receive buffer for 32 chars
+	
+		
+setup_ATtiny_HW;													//Configures USI as a transmitter
+
+DDRA |= 1 << DDA2;
+PORTA &= (~(1 << PA2));
+
+	//sei();
+	//String_from_USI (char_buffer);									//Wait while user changes baud rate if required
+	Char_from_USI();
+	//while(1){Tx_data_byte(Keypress);}
+
+/************Get Rx values using LED****************************************/
+
+
+/*while(1){
+	//for (int n = 0; n <= 4; n++){
+	Keypress = Char_from_USI ();
+	//if (Keypress == 'A') {PORTA |= (1 << PA2);  Timer_T1_sub();}
+		Tx_data_byte(Keypress);
+	Rx_clock += 1;
+	}*/
+
+/************Get Tx values using PC screen****************************************/
+	//while(1){Tx_data_byte('A');
+	/*for (int n = 0; n <= 4; n++){Tx_data_byte('A');
+	String_from_USI (char_buffer);
+	//Tx_clock += 1;
+	}*/
+	
+	set_device_type_and_memory_size;									//confirm device type and set EEPROM size
+	String_to_USI("\r\n\r\nProgram running on ");
+	String_to_USI (Device_family[family_ptr]);
+	String_to_USI (Device_type[device_ptr]);
+		
+	newline();
+			
+		String_to_USI
+		("\r\nEcho single keypresses ~ to escape.\r\n");				//Receive individual keypresses from PC
+		while ((Keypress = Char_from_USI ()) != '~'){					//and echo to screen (~ is escape char)
+		Tx_data_byte(Keypress);}										
+		
+		newline();
+		
+		while(1){
+		String_from_USI (char_buffer);
+		Echo_string(char_buffer);}
+		
+	return 1;}
+
+
+
+	
+		
+	
+	
+	
+/************************************************************************************************/
+void Timer_T1_sub(void){
+	TCNT1 = 0xF0BE;
+	TIFR1 = 0xFF;
+	TCCR1B = 5;
+	while(!(TIFR1 & (1<<TOV1))){if (TCNT1 >= 0xF85F)PORTA &= (~(1 << PA2));}
+	TIFR1 |= (1<<TOV1);
+TCCR1B = 0;}
