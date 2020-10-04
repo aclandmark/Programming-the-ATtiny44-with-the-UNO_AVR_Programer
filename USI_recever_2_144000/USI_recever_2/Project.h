cec807bf1ec121ed2272cf3ee36ee0aefc3d6c22
aclandmark <mark.osborne15@btinternet.com>
2020-10-04 20:06:28 +0100

    Update Project.h

--- USI_recever_2_144000/USI_recever_2/Project.h
+++ USI_recever_2_144000/USI_recever_2/Project.h
@@ -1,9 +1,9 @@
 
 //Baud rate set for 38400: replace 38400 with wanted baud rate
 
-#define Tx_clock						Tx_clock_38400
-#define Rx_clock						Rx_clock_38400
-#define Start_clock						Start_clock_38400
+#define Tx_clock						Tx_clock_256000
+#define Rx_clock						Rx_clock_256000
+#define Start_clock						Start_clock_256000
 
 #include "../Resources/USI_header.h"
 #include "../Resources/USI_subroutines.c"
