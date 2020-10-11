26d9e1870638ade7dfa937c522ebf1e28563f76c
aclandmark <mark.osborne15@btinternet.com>
2020-10-11 15:24:33 +0100

    Create Project_44.h

---
+++ Cal_ATtiny_461/Cal_ATtiny_461/Project_44.h
@@ -0,0 +1,39 @@
+
+
+
+//Baud rate set for 38400: replace 38400 with wanted baud rate
+
+#define Tx_clock						Tx_clock_38400
+#define Rx_clock						Rx_clock_38400
+#define Start_clock						Start_clock_38400
+
+
+
+#define	TCNT0				TCNT0L
+#define	TIFR0				TIFR
+#define	WDTCSR				WDTCR
+#define	PCIF0				PCIF
+#define TIMSK0				TIMSK
+#define TIFR1				TIFR
+#define PCINT0_vect			PCINT_vect
+#define TIM0_COMPA_vect		TIMER0_COMPA_vect
+#define DI_pin				0
+#define DO_pin				1
+#define USCK_pin			2
+#define setup_IO			setup_IO_461
+
+
+
+
+
+#include "../Resources/ATtiny_cal.h"
+#include "../Resources/USI_subroutines.c"
+#include "../Resources/ASKII_and_timer_subroutines.c"
+#include "../Resources/cal_subroutines.c"
+
+
+
+
+
+
+
