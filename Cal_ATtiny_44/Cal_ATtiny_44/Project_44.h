f63da52978407f60494dba7634af07f822809907
aclandmark <mark.osborne15@btinternet.com>
2020-10-11 15:23:03 +0100

    Update Project_44.h

--- Cal_ATtiny_44/Cal_ATtiny_44/Project_44.h
+++ Cal_ATtiny_44/Cal_ATtiny_44/Project_44.h
@@ -3,6 +3,10 @@
 
 //Baud rate set for 38400: replace 38400 with wanted baud rate
 
+
+
+
+
 #define Tx_clock						Tx_clock_38400
 #define Rx_clock						Rx_clock_38400
 #define Start_clock						Start_clock_38400
@@ -16,6 +20,15 @@
 #include "../Resources/USI_subroutines.c"
 #include "../Resources/ASKII_and_timer_subroutines.c"
 #include "../Resources/cal_subroutines.c"
+
+
+
+const char message_1[] PROGMEM = {"\r\n\r\nCalibrating ATtiny"};
+const char message_2[] PROGMEM = "\r\nDV/WV, previous OSCCAL values  ";
+const char message_3[] PROGMEM = "User cal? Enter 1 to F then x if OK\r\n";
+
+
+
 
 
 
