daea763b909d8aff4d33d4b9cb0aacae350b23ba
aclandmark <mark.osborne15@btinternet.com>
2020-10-20 09:21:55 +0100

    Update Project.h

--- Cal_ATtiny_44_461/Cal_ATtiny_44_461/Project.h
+++ Cal_ATtiny_44_461/Cal_ATtiny_44_461/Project.h
@@ -3,9 +3,9 @@
 
 //Baud rate set for 38400: replace 38400 with wanted baud rate (Application only not the programmer)
 
-#define Tx_clock						Tx_clock_38400
-#define Rx_clock						Rx_clock_38400
-#define Start_clock						Start_clock_38400
+#define Tx_clock						Tx_clock_19200
+#define Rx_clock						Rx_clock_19200
+#define Start_clock						Start_clock_19200
 
 
 
