dba06aa4619786cf22696c1936e305ac8328d4ca
aclandmark <mark.osborne15@btinternet.com>
2020-10-23 09:02:21 +0100

    Update USI_subroutines.c

--- USI_recever_2_144000/Resources/USI_subroutines.c
+++ USI_recever_2_144000/Resources/USI_subroutines.c
@@ -1,5 +1,46 @@
 
 
+
+#define receiveBit \
+while (!(TIFR0 & (1 << OCF0A)));\
+USICR |= (1 << USICLK);\
+TIFR0 |= (1 << OCF0A);\
+OCR0A +=  Rx_clock;
+
+/*
+Waits for T0 to count to the first clock period
+Triggers the USI clock
+Clears the compare match flag 
+Increments OCR0A by one clock period
+*/
+
+
+
+#define transmitBit \
+while (!(TIFR0 & (1 << OCF0A)));\
+USICR |= (1 << USICLK);\
+TIFR0 |= (1 << OCF0A);\
+OCR0A +=  Tx_clock;
+
+
+/***********************************************************************/
+
+#define fetch_char \
+TCNT0 = 0;\
+OCR0A = Rx_clock/2;\
+Start_clock;\
+USICR = (1 << USIWM0);\
+receiveBit;receiveBit;receiveBit;receiveBit;\
+receiveBit;receiveBit;receiveBit;receiveBit;\
+receiveBit;\
+keypress =  USIDR;\
+receiveBit;\
+TCCR0B = 0;
+
+
+
+
+
 unsigned char ReverseByte (unsigned char x) {
 	x = ((x >> 1) & 0x55) | ((x << 1) & 0xAA);
 	x = ((x >> 2) & 0x33) | ((x << 2) & 0xCC);
@@ -10,16 +51,13 @@ return x;}
 
 /**************************************************************************************************************/
 void Initialise_USI_Tx (void)
-{	//DDRA &= (!(1 << DDA6));							//Put WPU on PA6 (DI pin)
-	//PORTA |= 1 << PA6;
-	WPU_on_DI_pin;
+{	WPU_on_DI_pin;
 	USICR = 0;										//Reset USI
-	TCCR0B = 0;
-	OCR0A = Tx_clock;
+	TCCR0B = 0;										//Ensure USI clock is halted
+	OCR0A = Tx_clock;								//Define clock period
 	TIFR0 = (1 << OCF0A);							//Clear spurious interrupts
 	TCNT0 = 0;										//Clear TCNT0
 	USICR |= (1 << USIWM0);							//Select USI 3-wire mode
-	//DDRA |= (1 << DDA5);							//Configure DO (PA5) as output
 	Configure_DO_pin_as_Output;
 	USIDR = 0xFF;									//Load USIDR with 0xFF
 USISR = 0xFF;}										//Clear spurious interrupt flags
@@ -28,11 +66,13 @@ USISR = 0xFF;}										//Clear spurious interrupt flags
 
 
 /**************************************************************************************************************/
-void Tx_data_byte(unsigned char Txdata){				//USI already initialised for transmit by default
+void Char_to_USI(unsigned char Txdata){				//USI already initialised for transmit by default
 	
 	Txdata = ReverseByte(Txdata);
 	
 	Start_clock;	
+	
+	//TCCR0B = Pre_scaler;
 	transmitBit;										//Initialise transmission
 	USIDR = (Txdata >> 2) | 0x80;						//Add start bit
 
@@ -47,83 +87,51 @@ void Tx_data_byte(unsigned char Txdata){				//USI already initialised for transm
 
 
 /************************************************************************************************************/
-unsigned char Char_from_USI (void)									//Receive char
-{unsigned char keypress;
+unsigned char Char_from_USI (char timeout)				//Receive char
+{int p = 8000;											//Time out variable
+	unsigned char keypress;
 	
-	//DDRA &= (~((1 << DDA4) | (1 << DDA5) | (1 << DDA6)));			//Set USI IO to WPU
-	//PORTA |= (1 << PA4) | (1 << PA5) | (1 << PA6);
 	set_USI_ports_to_WPU;
-	
-	USICR = 0;														//Reset USI
-	
-	//while(PINA & (1 << PA6));										//wait for start bit
-	while (!(start_bit));
-	
+	USICR = 0;											//Reset USI
+	if (timeout)										//Timeout required
+	while ((!(start_bit)) && (p--));					//Wait here for start bit or timeout
+	else
+	while (!(start_bit));								//Or just wait indefinitely for start bit
+	if(start_bit){	
 	fetch_char;	
-	keypress = ReverseByte(keypress);
-	Initialise_USI_Tx ();											//Leave USI ready to transmit char
-return (keypress);}													//Take char from buffer register
+	keypress = ReverseByte(keypress);}
+	else keypress = 0;									//Timeout occurred
+	Initialise_USI_Tx ();								//Leave USI ready to transmit char
+return (keypress);}										//Take char from buffer register
 
 
 
 
 
 /**********************************************************************************************************/
-void String_from_USI (unsigned char* string)						//Receive string from PC
+void String_from_USI (unsigned char* string)			//Receive string from PC
 { int p = 8000;
 	unsigned char keypress;
 	
-	//DDRA &= (~((1 << DDA4) | (1 << DDA5) | (1 << DDA6)));			//Set USI IO to WPU
-	//PORTA |= (1 << PA4) | (1 << PA5) | (1 << PA6);
 	set_USI_ports_to_WPU;
-	
-	USICR = 0;														//Reset USI
-	//while(PINA & (1 << PA6));										//wait for start bit
-	while (!(start_bit));
-	
-	fetch_char;														//Detects 1 start bit, 8 data bits and ONE stop bit
+	USICR = 0;											//Reset USI
+	while (!(start_bit));								//Wait for start bit 
+	fetch_char;											//Detects 1 start bit, 8 data bits and ONE stop bit
 	string[0] = keypress;
-	for(int n = 1; n <= 45; n++)									//Max permissible string length 44 chars
-	{USICR = 0;														//Reset USI
-	//while ((PINA & (1 << PA6)) && (p--));							//Wait for start bit for limited time only 
-	while 	((!(start_bit)) && (p--));
-	
-	
-	//if (!(PINA & (1 << PA6)))										//Start bit detected
+	for(int n = 1; n <= 45; n++)						//Max permissible string length 44 chars
+	{USICR = 0;											//Reset USI
+	while 	((!(start_bit)) && (p--));					//Wait here for next start bit or end of string 
 	if(start_bit)
-	
 		{fetch_char;
 		string[n] = keypress;
-		p = 8000;}													//Clear TCNT0
-	
-	else{string[n] = '\0';											//receiver times out: terminate string
+		p = 8000;}										//Reset timeout period
+	else{string[n] = '\0';								//receiver times out: terminate string
 	break;}}
 	
 	for(int n = 0; n <= 45; n++)
-	{if (string[n]){string[n] = ReverseByte(string[n]);}
+	{if (string[n])
+		{string[n] = ReverseByte(string[n]);}
 		else break;}
-	Initialise_USI_Tx ();}											//Leave USI ready to transmit char
+	Initialise_USI_Tx ();}								//Leave USI ready to transmit char
 
 
-
-/******************************************************************************************************/
-void newline(void){String_to_USI ("\r\n");}
-
-
-
-/*******************************************************************************************************/
-void String_to_USI(const char* s){									//Send predefined string to PC
-	int i = 0;
-	while(i < 200){
-		if(s[i] == '\0') break;
-	Tx_data_byte(s[i++]);} }
-	
-
-	
-	
-/*******************************************************************************************************/
-	void Echo_string(unsigned char* s){								//Send variable string to PC
-		int i = 0;
-		while(i < 200){
-			if(s[i] == '\0') break;
-		Tx_data_byte(s[i++]);} }
