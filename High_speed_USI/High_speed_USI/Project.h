1470e1aa62e49407d8c73a07b4d541e057ea0e1d
aclandmark <mark.osborne15@btinternet.com>
2020-10-26 15:55:24 +0000

    Update Project.h

--- High_speed_USI/High_speed_USI/Project.h
+++ High_speed_USI/High_speed_USI/Project.h
@@ -1,22 +1,37 @@
 
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+#include <avr/pgmspace.h>
+
+
+
+
 //Baud rate set for 38400: replace 38400 with wanted baud rate
 
-#define Tx_clock						Tx_clock_256000
-#define Rx_clock						Rx_clock_256000
-#define Start_clock						Start_clock_256000
-#define Half_Rx_clock					Half_Rx_clock_256000
+#define Tx_clock						Tx_clock_128000
+#define Rx_clock						Rx_clock_128000
+#define Start_clock						Start_clock_128000
+#define Half_Rx_clock					Half_Rx_clock_128000
+
+char buffer_size = 47;					//User can set this value to maximum string length to be sent from PC
 
 
-/*
-#define DDR_USI				DDRA
-#define PIN_USI				PINA
-#define PORT_USI			PORTA
+//Settings for ATtiny44 and 84 devices
+/******************************************************************************************
 #define DI_pin				6
 #define DO_pin				5
 #define USCK_pin			4
 #define setup_IO			setup_IO_44
-*/
+******************************************************************************************/
 
+
+
+//Settings for ATtiny461 and 861 devices
+/******************************************************************************************/
 #define TIFR0				TIFR
 #define TCNT0				TCNT0L
 #define WDTCSR				WDTCR
@@ -25,18 +40,13 @@
 #define DO_pin				1
 #define USCK_pin			2
 #define setup_IO			setup_IO_461
-#define DDR_USI				DDRB
-#define PIN_USI				PINB
-#define PORT_USI			PORTB
+/******************************************************************************************/
 
 
 
-
-
-
-//#define set_USI_ports_to_WPU \
-//DDR_USI &= (~((1 << USCK_pin) | (1 << DO_pin) | (1 << DI_pin)));\
-//PORT_USI |= (1 << USCK_pin) | (1 << DO_pin) | (1 << DI_pin);
+#define DDR_USI				DDRA
+#define PIN_USI				PINA
+#define PORT_USI			PORTA
 
 #define WPU_on_DI_pin \
 DDR_USI &= (!(1 << DI_pin));\
@@ -58,5 +68,13 @@ DDR_USI &= (~(1 << DO_pin));
 
 #include "../Resources/USI_header.h"
 #include "../Resources/USI_subroutines.c"
+#include "../Resources/IO_subroutines.c"
+#include "../Resources/Text_reader_subs.c"
 
-
+const char message_1[] PROGMEM = "\r\n\r\nProgram running on ";
+const char message_2[] PROGMEM = "\r\nEcho single keypresses: Return key to escape.\r\n";
+const char message_3[] PROGMEM = "\r\nSend strings to USI\r\n";
+const char message_4[] PROGMEM = "\r\nNo text!\r\n";
+const char message_5[] PROGMEM = "\r\nReading strings.";
+const char message_6[] PROGMEM = "\r\nString number? 0 to exit";
+const char message_7[] PROGMEM = "\r\nNo string!\r\n";
