5f124421c5253fa913df745c8d898560bbf44964
aclandmark <mark.osborne15@btinternet.com>
2020-10-29 09:11:42 +0000

    Update Project.h

--- Cal_ATtiny_44_461_V2/Cal_ATtiny_44_461_V2/Project.h
+++ Cal_ATtiny_44_461_V2/Cal_ATtiny_44_461_V2/Project.h
@@ -4,13 +4,13 @@
 //Baud rate set for 38400: replace 38400 with wanted baud rate (Application only not the programmer)
 //Options are 960, 1920, 38400, 57600 and 76800
 
-#define Tx_clock						Tx_clock_38400
-#define Rx_clock						Rx_clock_38400
-#define Start_clock						Start_clock_38400
+#define Tx_clock						Tx_clock_76800
+#define Rx_clock						Rx_clock_76800
+#define Start_clock						Start_clock_76800
 
 
 
-/******Use these definitions for the ATtiny 461 familay of devices*********/
+/******Use these definitions for the ATtiny 461 familay of devices*********
 
 #define	TCNT0							TCNT0L
 #define	TIFR0							TIFR
@@ -44,11 +44,11 @@
 #define DDR_USI							DDRA
 #define PIN_USI							PINA
 #define PORT_USI						PORTA
-/***************************************************************************/
+***************************************************************************/
 
 
 
-/******Use these definitions for the ATtiny 44 familay of devices*********
+/******Use these definitions for the ATtiny 44 familay of devices*********/
 
 #define set_PCI_mask_on_SCK			PCMSK0	= (1 << PCINT4)							//Used to calibrate internal clock
 #define clear_PCI_mask_on_SCK		PCMSK0 &= (~(1 << PCINT4))
@@ -72,7 +72,7 @@
 #define DDR_USI						DDRA
 #define PIN_USI						PINA
 #define PORT_USI					PORTA
-*******************************************************************************/
+/*******************************************************************************/
 
 
 #define enable_PCI_on_SCK_pin		GIFR |= 1 << PCIF0; GIMSK |= 1 << PCIE0;
